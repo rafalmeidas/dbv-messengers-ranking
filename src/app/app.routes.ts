4bf1fa4404bdf3ccfb428f134b566b7c7383ac09
Rafael Silva <ralfalmeidas21@gmail.com>
2025-05-06 20:37:43 -0300

    add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import * as _ from 'lodash';
 
-export const routes: Routes = [];
+import { coreRoutes } from './core/features/core.routes';
+import { authGuard } from './core/guards/auth.guard';
+
+export const routes: Routes = _.concat(coreRoutes, [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((c) => c.HomeComponent),
+    canActivate: [authGuard],
+  },
+]);
