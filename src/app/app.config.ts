23c68e6ceadf93e186e4683f42ba397d0f29b165
Rafael Silva <ralfalmeidas21@gmail.com>
2025-05-06 21:22:55 -0300

    wip deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideRouter } from '@angular/router';
 
-import { environment } from '../../environment';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
