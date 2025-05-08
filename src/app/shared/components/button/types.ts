d9c429fc5be6ec023e00157e42ff29beefb301f3
Rafael Silva <ralfalmeidas21@gmail.com>
2025-05-08 20:54:15 -0300

    change improving button component

--- src/app/shared/components/button/types.ts
+++ src/app/shared/components/button/types.ts
@@ -1,3 +1,5 @@
 export const ButtonVariants = ['icon', 'outline', 'primary', 'text'] as const;
-
 export type ButtonVariant = (typeof ButtonVariants)[number];
+
+export const ButtonIcons = [''] as const;
+export type ButtonIcon = (typeof ButtonIcons)[number];
