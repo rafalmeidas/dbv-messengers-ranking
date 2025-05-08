396991d0f255ffb113533c9040094e8d6e78517f
Rafael Silva <ralfalmeidas21@gmail.com>
2025-05-08 20:53:48 -0300

    change improving button component

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ButtonVariant } from './types';
+import { ButtonIcon, ButtonVariant } from './types';
 
 @Component({
   selector: 'app-button',
@@ -9,9 +9,20 @@ import { ButtonVariant } from './types';
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input() variant: ButtonVariant = 'primary';
   @Input() disabled = false;
+  @Input() icon?: ButtonIcon;
+  @Input() label?: string;
+  @Input() type: 'button' | 'submit' = 'button';
 
   @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
+
+  isOnlyIcon: boolean = false;
+  showLabel: boolean = false;
+
+  ngOnInit(): void {
+    this.isOnlyIcon = this.variant === 'icon';
+    this.showLabel = !this.isOnlyIcon && !!this.label;
+  }
 }
