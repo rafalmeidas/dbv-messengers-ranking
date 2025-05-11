5d54cfd599625b082717d3e25059ba58a436bf8d
Rafael Silva <ralfalmeidas21@gmail.com>
2025-05-11 14:38:22 -0300

    add list component

---
+++ src/app/shared/components/list/list.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-list',
+  imports: [CommonModule, RouterModule, ButtonComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent<T extends Record<string, any>> {
+  @Input() title: string = 'Title';
+  @Input() btnNewLabel: string = 'Novo';
+  @Input() btnNewUrl: string = '';
+  @Input() listArray: T[] = [];
+  @Input() labelKey: keyof T = 'name' as keyof T;
+  @Input() labelId: keyof T = 'id' as keyof T;
+  @Input() btnEditUrl: string = '';
+}
