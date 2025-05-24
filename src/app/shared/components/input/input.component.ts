2708463e731a3ad10f72be6d405f5f602ba0a168
Rafael Silva <ralfalmeidas21@gmail.com>
2025-05-24 20:48:39 -0300

    refactor input component

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ControlValueAccessorAbstractComponent } from '../abstracts/control-value-accessor.abstract.component';
+import { FormGroupComponent } from '../form-group/form-group.component';
 
 @Component({
   selector: 'app-input',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, FormGroupComponent],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
-export class InputComponent extends ControlValueAccessorAbstractComponent {
+export class InputComponent extends ControlValueAccessorAbstractComponent<string> {
   @Input() type: 'text' | 'password' | 'email' = 'text';
 }
