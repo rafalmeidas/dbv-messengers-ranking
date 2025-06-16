9535049fe382183eac9d75df3e26dbe32cc9fe74
Rafael Silva <ralfalmeidas21@gmail.com>
2025-06-16 19:18:46 -0300

    add styling in unauthorized page.

--- src/app/core/features/unauthorized/unauthorized.component.ts
+++ src/app/core/features/unauthorized/unauthorized.component.ts
@@ -1,11 +1,16 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-unauthorized',
   imports: [],
   templateUrl: './unauthorized.component.html',
-  styleUrl: './unauthorized.component.scss'
+  styleUrl: './unauthorized.component.scss',
 })
 export class UnauthorizedComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+  }
 }
